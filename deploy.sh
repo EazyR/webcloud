b486ac517fad09f4398e71fba3d0dcc69cef5cb4
PapinuttiGuillaume <g.papinutti@gmail.com>
2017-01-26 21:46:43 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -69,10 +69,10 @@ fi
 
 # Use the kiwix serve command to serve the .zim file on port 80 : Rajouter un if / else. Si le processus est déjà lancé, le tuer puis le relancer.
 ps cax | grep kiwi-server > /dev/null 
-if [ $? -eq 0 ]; 
-then
- echo "kiwix déjà lancé"
-else
-./kiwix/bin/kiwix-serve --port=81 wikipedia_fr_medicine_nopic_2016-10.zim
+if pidof kiwix-server >/dev/null; then
+    ./kiwix/bin/kiwix-serve --port=81 wikipedia_fr_medicine_nopic_2016-10.zim
  echo "kiwix vient d'être lancé"
+else
+ echo "kiwix déjà lancé"
 fi
+
